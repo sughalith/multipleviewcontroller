7d9d56687233ade65bf168fb0ae9a1a77f34c933
Arkadiusz Stankiewicz <arkadiuszstankiewicz@MacBook-Pro-test.local>
2017-10-03 14:29:08 +0200

    simple view controllers #3

---
+++ Multiple View Controllers/SecondViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SecondViewController.swift
+//  Multiple View Controllers
+//
+//  Created by Arkadiusz Stankiewicz on 03.10.2017.
+//  Copyright © 2017 Arkadiusz Stankiewicz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Second view controller working")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
